a2e2be15f158f3abebca107766a6322e8233506f
Dev Shrestha <dev5@uw.edu>
2019-01-09 08:46:42 -0800

    No test yet

--- Lab02-UnitTesting/Program.cs
+++ Lab02-UnitTesting/Program.cs
@@ -9,13 +9,15 @@ namespace Lab02_UnitTesting
            SuperFront();
         }
 
-        static int SuperFront()
+        public static void SuperFront()
         {
+            double Balance = 3000;
+
             bool Event = true;
             while(Event)
             {
 
-            double Balance = 3000;
+            
             Console.WriteLine("Hello! Welcome to your local Bank");
             Console.WriteLine("How can I help you today?");
             Console.WriteLine("1. View Balance");
@@ -44,31 +46,48 @@ namespace Lab02_UnitTesting
                     Console.WriteLine("How much would you like to withdraw");
                     double withDrawAmt; 
                     double.TryParse(Console.ReadLine(), out withDrawAmt);
-                    WithDraw(withDrawAmt);
+                    WithDraw(withDrawAmt, Balance);
                     Console.WriteLine($"Your new balance is {Balance}");
+                    break;
 
                     case 3:
                     Console.WriteLine("How much would you like to deposit");
                     double depositAmt;
                     double.TryParse(Console.ReadLine(), out depositAmt);
-                    Deposit(depositAmt);        
+                    Deposit(depositAmt, Balance);        
                     Console.WriteLine($"Your new balance is {Balance}");
-                    
+                    break;
+
                     case 4:
                     Console.WriteLine("Thank you for your business");
                     Event = false;
-        
+                    Environment.Exit(0);
+                    break;
+
                     default:
-                    //Console.WriteLine("Invalid selection. Please select 1, 2, 3 or 4.");
+                    Console.WriteLine("Invalid selection. Please select 1, 2, 3 or 4.");
                     break;
                 }
-            return;
-        
+
+                Console.WriteLine("Please select from the following options:");
+                Console.WriteLine("1. Go to Main Menu");
+                Console.WriteLine("4. Exit");
+                int newSelect = Int32.Parse(Console.ReadLine());
+                if (newSelect == 1)
+                {
+                    newSelect = userInput;
+
+                }
+                else if (newSelect == 4)
+                {
+                    Environment.Exit(0);
+                }
+
             }
 
          }
         
-        public static string WithDraw(double withDrawAmt)
+        public static string WithDraw(double withDrawAmt, double Balance)
         {
             if(withDrawAmt > 0)
             {
@@ -78,13 +97,13 @@ namespace Lab02_UnitTesting
                 return "Withdraw success";          
         }
 
-        public static string Deposit(double depositAmt)
+        public static string Deposit(double depositAmt, double Balance)
         {
             if(depositAmt < 0)
             {
                 Console.WriteLine("Can't deposit, try withdraw");
             }           
-                Balance +=withDrawAmt; 
+                Balance += depositAmt; 
                 return "Deposit success";            
         }
 
